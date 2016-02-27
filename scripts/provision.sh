5d13a10d011211533d8a12fbda91e498bb3e57e0
Alvaro Miranda <kikitux@gmail.com>
2016-02-27 23:39:17 +1300

    refactor provision.sh

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -7,12 +7,12 @@ PACKAGES="curl git bzr"
 sudo -E -H apt-get update
 sudo -E -H apt-get install -y -q --no-install-recommends ${PACKAGES}
 
-[ -f go1.6.linux-amd64.tar.gz ] || {
-  curl -o go1.6.linux-amd64.tar.gz https://storage.googleapis.com/golang/go1.6.linux-amd64.tar.gz
-}
+unset gover
+gover=1.6
 
-sudo tar -C /usr/local -xzf go1.6.linux-amd64.tar.gz
-[ -f go1.6.linux-amd64.tar.gz ] && rm go1.6.linux-amd64.tar.gz
+curl -o go${gover}.linux-amd64.tar.gz https://storage.googleapis.com/golang/go${gover}.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go${gover}.linux-amd64.tar.gz
+[ -f go${gover}.linux-amd64.tar.gz ] && rm go${gover}.linux-amd64.tar.gz
 
 grep 'export GOROOT=' .bash_profile || ( echo export GOROOT=/usr/local/go | tee -a .bash_profile )
 grep 'export GOPATH=' .bash_profile || ( echo export GOPATH=/vagrant/go | tee -a .bash_profile )
