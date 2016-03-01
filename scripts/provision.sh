6178ee1f624606c918789342906e0a33be28ce57
Alvaro Miranda <kikitux@z2-3.kikitux.net>
2016-02-29 23:24:19 -0700

    added vim, fix error on Makefile

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -4,11 +4,12 @@ set -e
 export DEBIAN_FRONTEND=noninteractive
 unset PACKAGES
 
-PACKAGES="curl git bzr"
+PACKAGES="curl git git vim"
 sudo -E apt-get clean
 sudo -E -H apt-get update
 sudo -E -H apt-get install -y -q --no-install-recommends ${PACKAGES}
 
+#go
 unset gover
 gover=1.6
 
@@ -21,6 +22,28 @@ grep 'export GOPATH=' .bash_profile || ( echo export GOPATH=/vagrant/go | tee -a
 source .bash_profile
 grep 'export PATH=' .bash_profile || ( echo export PATH=$PATH:$GOROOT/bin:$GOPATH/bin | tee -a .bash_profile)
 source .bash_profile
+
+#vim
+[ -f ~/.vim/autoload/pathogen.vim ] || {
+  mkdir -p ~/.vim/autoload
+  curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+}
+
+[ -d ~/.vim/bundle/vim-go ] || {
+  mkdir -p ~/.vim/bundle
+  git clone https://github.com/fatih/vim-go.git ~/.vim/bundle/vim-go
+}
+
+grep 'execute pathogen#infect()' ~/.vimrc &>/dev/null || {
+cat > ~/.vimrc <<EOF
+execute pathogen#infect()
+syntax on
+filetype plugin indent on
+EOF
+}
+
+sed -i '/tty/!s/mesg n/tty -s \&\& mesg n/' /home/vagrant/.profile
+sudo sed -i '/tty/!s/mesg n/tty -s \&\& mesg n/' /root/.profile
 
 sudo -E -H apt-get clean
 [ -f /etc/udev/rules.d/70-persistent-net.rule ] && sudo rm -f /etc/udev/rules.d/70-persistent-net.rule || true
